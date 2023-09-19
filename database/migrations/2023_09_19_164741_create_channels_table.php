af1885f48ea35279e79978d3d6ed1362d518f5b3
Nacho <italiyankina@gmail.com>
2023-09-19 17:51:33 +0100

    Replace channel table fields

--- database/migrations/2023_09_19_164741_create_channels_table.php
+++ database/migrations/2023_09_19_164741_create_channels_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('channels', function (Blueprint $table) {
-            $table->id();
+            $table->id();            
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('color');
             $table->timestamps();
         });
     }
