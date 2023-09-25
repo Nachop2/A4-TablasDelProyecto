43b299c3cbe52354a1e30f3b4d4201586e97f0e3
Nacho <italiyankina@gmail.com>
2023-09-25 19:20:57 +0100

    Create a relationship between tables, to add creators to link

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -4,7 +4,7 @@
 
 
 @foreach ($links as $link)
-<li>{{$link->title}}</li>
+<li>{{$link->title}} <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small></li>
 @endforeach
 {{$links->links()}}
 
