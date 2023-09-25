7951e1ed4d4b45f97c9a612fcd2e17aa8faf1fb8
Nacho <italiyankina@gmail.com>
2023-09-25 19:01:21 +0100

    Resolve DatabaseSeeder dependencies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\CommunityLink;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
