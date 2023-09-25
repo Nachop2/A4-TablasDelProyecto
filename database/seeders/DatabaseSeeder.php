e53ac0f3323d783260ec60854f48a70e669007f8
Nacho <italiyankina@gmail.com>
2023-09-25 18:49:19 +0100

    Add run method to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::delete('delete from community_links');
+        CommunityLink::factory()->count(50)->create();
     }
 }
