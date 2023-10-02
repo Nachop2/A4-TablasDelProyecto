e17356b123c742f743e0c403a70de8d2d5d8ab4b
Nacho <italiyankina@gmail.com>
2023-10-02 20:42:30 +0100

    Separate link column from index

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -3,19 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        {{-- Left colum to show all the links in the DB --}}
-        <div class="col-md-8">
-            <h1>Community</h1>
-            @foreach ($links as $link)
-            <li>
-                <a href="{{ $link->link }}" target="_blank">
-                    {{ $link->title }}
-                </a>
-                <small>Contributed by: {{ $link->creator->name }} {{ $link->updated_at->diffForHumans() }}</small>
-            </li>
-            @endforeach
-
-        </div>
+        @include('community.link-column')
         @include('community.add-link')
     </div>
     {{ $links->links() }}
---
+++ resources/views/community/link-column.blade.php
@@ -0,0 +1,13 @@
+{{-- Left colum to show all the links in the DB --}}
+<div class="col-md-8">
+    <h1>Community</h1>
+    @foreach ($links as $link)
+    <li>
+        <a href="{{ $link->link }}" target="_blank">
+            {{ $link->title }}
+        </a>
+        <small>Contributed by: {{ $link->creator->name }} {{ $link->updated_at->diffForHumans() }}</small>
+    </li>
+    @endforeach
+
+</div>
