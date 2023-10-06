965611fabe4251db344000307080b9d9096d9bf4
Nacho <italiyankina@gmail.com>
2023-10-06 20:56:35 +0100

    Add jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,14 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+
+    protected $commands = [
+
+        Commands\DispatchTestJob::class,
+
+    ];
+
     /**
      * Define the application's command schedule.
      */
@@ -20,7 +28,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
