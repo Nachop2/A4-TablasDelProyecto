1eadd1a5b6712e6994fcd9894fbde74517ce53d4
Nacho <italiyankina@gmail.com>
2023-12-01 17:42:21 +0000

    add authentication to api

--- app/Http/Controllers/Api/V1/CommunityLinkController.php
+++ app/Http/Controllers/Api/V1/CommunityLinkController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class CommunityLinkController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['index', 'show']]);
+    }
+
+
     /**
      * Display a listing of the resource.
      */
